c0b91ab97ca3b10421202bcacd3c6979a1b998d1
Emma Craig <emma.ryal@gmail.com>
2020-11-05 14:18:18 +0100

    faq pages + ready for initial presentation

---
+++ scripts/Faqs.js
@@ -0,0 +1,43 @@
+"use strict"
+
+function showAnswer1() {
+    const answer1 = document.getElementById('q1-div');
+    
+    if (answer1.style.display === "none") {
+        answer1.style.display = "block";
+    } else {
+        answer1.style.display = "none";
+    }
+ }
+
+ function showAnswer2() {
+    const answer1 = document.getElementById('q2-div');
+    
+    if (answer1.style.display === "none") {
+        answer1.style.display = "block";
+    } else {
+        answer1.style.display = "none";
+    }
+ }
+
+ function showAnswer3() {
+    const answer1 = document.getElementById('q3-div');
+    
+    if (answer1.style.display === "none") {
+        answer1.style.display = "block";
+    } else {
+        answer1.style.display = "none";
+    }
+ }
+
+ function showAnswer4() {
+    const answer1 = document.getElementById('q4-div');
+    
+    if (answer1.style.display === "none") {
+        answer1.style.display = "block";
+    } else {
+        answer1.style.display = "none";
+    }
+ }
+
+ 
